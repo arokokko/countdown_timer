b1a2dde74bb448832695bf9ee4d136966ccecddc
ALeks <arokokko@gmail.com>
2020-02-20 11:31:21 +0700

    Added function concotination 0 if number is less than 10 and added comments

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict';
 
-    let deadLine = '2020-02-21';    /* дата окончания срока */
+    let deadLine = '2020-02-22';    /* устанавливаем дату окончания срока */
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),   /* количество миллисекунд между окончанием срока и текущей датой */
         seconds = Math.floor((t /1000) % 60),              /* количество оставшихся секунд до 60 */
         minutes = Math.floor((t /1000 /60) % 60),           /* количество оставшихся минут до 60*/
-        hours = Math.floor(t /(1000 * 60 * 60));
+        hours = Math.floor(t /(1000 * 60 * 60));            /* количество оставшихся часов*/
+        // hours = Math.floor(t /(1000 * 60 * 60) % 24),
+        // days = Math.floor(t /(1000 * 60 * 60 * 24));
 
         return {
-            'total': t,
+            'total': t,               /* функция возвращает объект с записанными данными */
             'seconds': seconds,
             'minutes': minutes,
             'hours': hours
@@ -18,22 +20,31 @@ window.addEventListener('DOMContentLoaded', function(){
     }
 
     function setClock(id, endtime) {
-        let timer = document.getElementById(id),
-            hours = document.querySelector(".hours"),
-            minutes = document.querySelector('.minutes'),
-            seconds = document.querySelector('.seconds'),
-            timeInterval = setInterval(clockUpdate, 1000);
+        let timer = document.getElementById(id),         /* получаем данные из верстки */
+            hours = timer.querySelector(".hours"),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(clockUpdate, 1000);   /* запускаем функцию обновления раз в секунду */
             
 
-        function clockUpdate() {
+        function clockUpdate() {                   /* создаем динамическую функцию которая сначала получает данные о времени  */
             let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+            let addZero;      
 
-            if (t.total <= 0) {
+            if (t.total <= 0) {                       /*  отключаем таймер когда выходит время */
                 clearInterval(timeInterval);
+            } else {
+                addZero = function(num) {              /* функция добавления 0 когда секунд или минут меньше 10 */
+                    if(num < 10) {
+                        return ('0' + num);
+                    } else {
+                        return num;
+                    }
+                };
             }
+            hours.textContent = addZero(t.hours);          /* а потом перезаписывает данные в верстку */
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
         }
     }
 
